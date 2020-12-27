550f3cc6a3dad413575e60cf49990edff94324c7
Alvaro <alvaro@localhost.localdomain>
2020-12-27 16:45:42 +0100

    Update emacs conf

--- emacs/.emacs.d/conf.el
+++ emacs/.emacs.d/conf.el
@@ -1,10 +1,10 @@
+
 (require 'package)
 
-(setq package-list '(evil rust-mode magit fzf evil xclip
-			  linum-relative lsp-ui clang-format
-			  ace-window use-package xcscope evil-tabs org
-			  helm evil-leader elpy projectile rust-mode racer
-			  company  typescript-mode ccls fold-this))
+(setq package-list '(evil rust-mode magit fzf evil xclip linum-relative lsp-ui
+                          ace-window use-package org helm evil-leader elpy
+                          projectile rust-mode racer company ccls ivy swiper
+                          counsel))
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
@@ -29,23 +29,19 @@
 (require 'lsp-ui)
 (add-hook 'lsp-mode-hook 'lsp-ui-mode)
 (add-hook 'after-init-hook 'global-company-mode)
+(setq lsp-diagnostic-package :none)
 
 ;; ccls
-
-;; (require 'ccls)
 (setq ccls-executable "/usr/bin/ccls")
-
 (use-package ccls
   :hook ((c-mode c++-mode objc-mode cuda-mode) .
          (lambda () (require 'ccls) (lsp))))
-
 
 (defface company-box-candidate
   '((((background light)) :foreground "black")
     (t :foreground "white"))
   "Face used to color candidates."
   :group 'company-box)
-
 
 (package-initialize)
 
@@ -57,7 +53,6 @@
 (defalias 'list-buffers 'ibuffer)
 
 ;; windows
-
 (use-package ace-window
   :ensure t
   :init
@@ -68,14 +63,10 @@
 ;; magit config
 (global-set-key (kbd "C-x g") 'magit-status)
 
-
 ;; evil config
-
 (setq evil-toggle-key "C-q")
 (require 'evil)
 (evil-mode 1)
-
-(global-evil-tabs-mode t)
 
 (eval-after-load 'evil-maps
   '(define-key evil-normal-state-map (kbd "M-.") nil))
@@ -91,15 +82,12 @@
   "x" 'helm-M-x)
 
 (define-key evil-normal-state-map (kbd "C-]") 'xref-find-definitions)
-(define-key evil-normal-state-map (kbd "C-[") 'find-tag)
 (define-key evil-normal-state-map (kbd "C-r") 'xref-find-references)
-;; (define-key evil-normal-state-map (kbd "C-f") 'ggtags-find-definition)
 (define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
 
 ;; fuzzy file find
 
-(global-set-key (kbd "C-c p p") 'fzf-git)
-
+(global-set-key (kbd "C-c p p") 'counsel-fzf)
 
 ; Get rid of the startup message
 (setq inhibit-startup-message t)
@@ -117,7 +105,6 @@
 
 (add-hook 'c-mode-common-hook #'hs-minor-mode)
 
-
 ;; hide menu bar
 (menu-bar-mode -1)
 
@@ -127,14 +114,9 @@
 		    :background "Black"
 		    :box nil)
 
-;; cscope
-(require 'xcscope)
-(cscope-setup)
-
 (add-hook 'dired-mode-hook (lambda () (evil-emacs-state)))
 
 ;; clipboard
-
 (require 'xclip)
 (xclip-mode 1)
 
@@ -146,9 +128,7 @@
           tab-width 2)
 
 ;; text mode
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
-(add-hook 'text-mode-hook
-  '(lambda() (set-fill-column 80)))
+(setq set-fill-column 80)
 
 ;; c-h with backspace issues
 
@@ -168,19 +148,11 @@
 (add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1)))
 (setq elpy-rpc-python-command "python3")
 
-;; clang-format
-;; (require 'clang-format)
-;; (global-set-key (kbd "C-c i") 'clang-format-region)
-;; (global-set-key (kbd "C-c u") 'clang-format-buffer)
-
-(setq clang-format-style-option "LLVM")
-
 ;; rust
 (require 'rust-mode)
 
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
-
 (add-hook 'racer-mode-hook #'company-mode)
 
 (global-set-key (kbd "C-c d") "M-.")
@@ -188,10 +160,7 @@
 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 (setq company-tooltip-align-annotations t)
 
-; (setq indent-tabs-mode nil)
-; (setq c-default-style
-      ; '((c++-mode . "k&r") (awk-mode . "awk") (other . "gnu")))
-(setq-default tab-width 2) ; set tab width to 4 for all buffers
+(setq-default tab-width 4) 
 (when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
 
 (defun indent-or-complete ()
@@ -203,22 +172,130 @@
 ;; linum-relative
 (require 'linum-relative)
 
-;; fold-this
-(global-set-key (kbd "C-c C-f") 'fold-this)
-(global-set-key (kbd "C-c C-u") 'fold-this-unfold-all)
-
 ;; Custom themes
 (add-to-list 'custom-theme-load-path "themes")
-(load-theme 'alect-dark t)
-
+;; (load-theme 'vscode-dark-plus t)
 
 ;; enable mouse
-;; scroll one line at a time (less "jumpy" than defaults)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 5))) ;; one line at a time
-(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(require 'mouse)
+(xterm-mouse-mode t)
+(mouse-wheel-mode t)
 
-(xterm-mouse-mode 1)
+(global-set-key [(C-f5)] 'compile)
 
-(global-set-key [mouse-4] 'scroll-down-line)
-(global-set-key [mouse-5] 'scroll-up-line)
+(add-hook 'c-mode-common-hook (lambda ()
+      (local-set-key (kbd "RET") 'newline-and-indent)))
+
+(defun win-resize-top-or-bot ()
+  "Figure out if the current window is on top, bottom or in the
+middle"
+  (let* ((win-edges (window-edges))
+	 (this-window-y-min (nth 1 win-edges))
+	 (this-window-y-max (nth 3 win-edges))
+	 (fr-height (frame-height)))
+    (cond
+     ((eq 0 this-window-y-min) "top")
+     ((eq (- fr-height 1) this-window-y-max) "bot")
+     (t "mid"))))
+
+(defun win-resize-left-or-right ()
+  "Figure out if the current window is to the left, right or in the
+middle"
+  (let* ((win-edges (window-edges))
+	 (this-window-x-min (nth 0 win-edges))
+	 (this-window-x-max (nth 2 win-edges))
+	 (fr-width (frame-width)))
+    (cond
+     ((eq 0 this-window-x-min) "left")
+     ((eq (+ fr-width 4) this-window-x-max) "right")
+     (t "mid"))))
+
+(defun win-resize-enlarge-horiz ()
+  (interactive)
+  (cond
+   ((equal "top" (win-resize-top-or-bot)) (enlarge-window -5))
+   ((equal "bot" (win-resize-top-or-bot)) (enlarge-window 5))
+   ((equal "mid" (win-resize-top-or-bot)) (enlarge-window -5))
+   (t (message "nil"))))
+
+(defun win-resize-minimize-horiz ()
+  (interactive)
+  (cond
+   ((equal "top" (win-resize-top-or-bot)) (enlarge-window 5))
+   ((equal "bot" (win-resize-top-or-bot)) (enlarge-window -5))
+   ((equal "mid" (win-resize-top-or-bot)) (enlarge-window 5))
+   (t (message "nil"))))
+
+(defun win-resize-enlarge-vert ()
+  (interactive)
+  (cond
+   ((equal "left" (win-resize-left-or-right)) (enlarge-window-horizontally -5))
+   ((equal "right" (win-resize-left-or-right)) (enlarge-window-horizontally 5))
+   ((equal "mid" (win-resize-left-or-right)) (enlarge-window-horizontally -5))))
+
+(defun win-resize-minimize-vert ()
+  (interactive)
+  (cond
+   ((equal "left" (win-resize-left-or-right)) (enlarge-window-horizontally 5))
+   ((equal "right" (win-resize-left-or-right)) (enlarge-window-horizontally -5))
+   ((equal "mid" (win-resize-left-or-right)) (enlarge-window-horizontally 5))))
+
+(global-set-key [C-M-down] 'win-resize-minimize-vert)
+(global-set-key [C-M-up] 'win-resize-enlarge-vert)
+(global-set-key [C-M-left] 'win-resize-minimize-horiz)
+(global-set-key [C-M-right] 'win-resize-enlarge-horiz)
+(global-set-key [C-M-up] 'win-resize-enlarge-horiz)
+(global-set-key [C-M-down] 'win-resize-minimize-horiz)
+(global-set-key [C-M-left] 'win-resize-enlarge-vert)
+(global-set-key [C-M-right] 'win-resize-minimize-vert)
+
+(use-package counsel
+  :ensure t
+  :bind
+  (("M-y" . counsel-yank-pop)
+   :map ivy-minibuffer-map
+   ("M-y" . ivy-next-line)))
+
+(use-package ivy
+  :ensure t
+  :diminish (ivy-mode)
+  :bind (("C-x b" . ivy-switch-buffer))
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "%d/%d ")
+  (setq ivy-display-style 'fancy))
+
+
+(use-package swiper
+  :ensure t
+  :bind (("C-c C-s" . swiper-isearch)
+         ("C-c C-r" . ivy-resume)
+         ("M-x" . counsel-M-x))
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-display-style 'fancy)
+    ))
+
+(icomplete-mode 1)
+
+(add-hook 'eshell-mode-hook
+          (lambda ()
+            ;; (setq pcomplete-ignore-case t)
+            ;; (setq pcomplete-cycle-completions nil)
+
+            ;; These have to be done in a hook because
+            ;; eshell-command-map is a buffer local variable.  It's
+            ;; apparently not supposed to be and appears to to be a
+            ;; bug in the way eshell is implemented.
+            ;; eshell-mode-map:    <key>
+            ;; ehsell-command-map: C-c <key>
+            (define-key eshell-command-map (kbd "C-t") 'eshell-truncate-buffer-all)
+            (define-key eshell-command-map (kbd "M-o") 'eshell-truncate-buffer-all)
+            (define-key eshell-mode-map (kbd "M-r") 'counsel-esh-history)
+            (define-key eshell-mode-map (kbd "<tab>")
+              (lambda () (interactive) (pcomplete-std-complete)))
+            )
+          )
